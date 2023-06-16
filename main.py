d341eca2d9416c2947dc2ae824e844f07ce660cf
Szymon120 <szymonpacyga21@gmail.com>
2023-06-16 15:34:53 +0200

    Changes in GUI appearence

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ import numpy as np
 def donothing():
    pass
 
-# okno?
+# okno
 root = tk.Tk()
 root.title("Dziennik ciśnieniowca")
 main_db = Database()
-#menu głowne
+# menu głowne
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 # new_file(show_frame)
@@ -36,9 +36,15 @@ menubar.add_cascade(label="Help", menu=helpmenu)
 main_frame = tk.Frame(root, padx=10, pady=10)
 main_frame.pack()
 
+frame_left = tk.Frame(main_frame, padx=10, pady=10)
+frame_left.grid(row=0, column=0)
+
+frame_right = tk.Frame(main_frame, padx=10, pady=10)
+frame_right.grid(row=0, column=1)
+
 # frame do wprowadzania pomiarów
-pressure_input_frame = tk.LabelFrame(main_frame, text="Wprowadzanie nowych pomiarów ciśnienia", padx=10, pady=10)
-pressure_input_frame.pack()
+pressure_input_frame = tk.LabelFrame(frame_left, text="Wprowadzanie nowych pomiarów ciśnienia", padx=10, pady=10)
+pressure_input_frame.grid(row=0, column=0)
 # ciśnienie skurczowe label i entry
 label_systolic_pressure = tk.Label(pressure_input_frame, text="Ciśnienie skurczowe:", anchor="w")
 label_systolic_pressure.grid(row=0, column=0, sticky="w")
@@ -110,8 +116,8 @@ button_remove_last_data_entry = tk.Button(pressure_input_frame, text="Usuń wcze
 button_remove_last_data_entry.grid(row=6, column=0, sticky="w"+"e", columnspan=3, pady=5)
 
 # frame do szukania pomiarów
-search_measure_frame = tk.LabelFrame(main_frame, text="Wyszukaj pomiar ciśnienia", padx=10, pady=10)
-search_measure_frame.pack(fill='x')
+search_measure_frame = tk.LabelFrame(frame_left, text="Wyszukaj pomiar ciśnienia", padx=10, pady=10)
+search_measure_frame.grid(row=1, column=0, sticky="w"+"e")
 
 search_by_date_frame = tk.LabelFrame(search_measure_frame, text="Wyszukaj po dacie:")
 search_by_date_frame.grid(row=0, column=0)
@@ -131,7 +137,7 @@ entry_date_2.grid(row=1, column=0)
 for widget in search_by_date_frame.winfo_children():
     widget.grid_configure(padx=10, pady=5)
 
-search_type_combobox = ttk.Combobox(search_by_value_frame, values=[" ","Data", "Ciśnienie skurczowe", "Ciśnienie rozkurczowe", "Tętno"])
+search_type_combobox = ttk.Combobox(search_by_value_frame, values=[" ", "Data", "Ciśnienie skurczowe", "Ciśnienie rozkurczowe", "Tętno"])
 search_type_combobox.grid(row=0, column=0)
 
 entry_type_value = tk.Entry(search_by_value_frame)
@@ -149,30 +155,30 @@ def change_variable():
         search_db(show_frame,search_type_combobox.get(), search_variable, show_frame_text)
 
 
-button_data_entry = tk.Button(search_measure_frame, text="Szukaj", command= lambda: change_variable())
+button_data_entry = tk.Button(search_measure_frame, text="Szukaj",pady=20, command=lambda: change_variable())
 button_data_entry.grid(row=0, column=2)
 
 #Plot options
 plot_dp = IntVar()
 plot_sp = IntVar()
 plot_ht = IntVar()
-plot_options = tk.LabelFrame(main_frame, text="Opcje wykresu", padx=10, pady=10)
-plot_options.pack(fill='both')
-checkbox_show_sp = tk.Checkbutton(plot_options, text='Pokazuj Ciś. Sk.',variable=plot_sp, onvalue=1, offvalue=0)
-checkbox_show_sp.pack()
-checkbox_show_dp = tk.Checkbutton(plot_options, text='Pokazuj Ciś. Roz.',variable=plot_dp, onvalue=1, offvalue=0)
-checkbox_show_dp.pack()
-checkbox_show_ht = tk.Checkbutton(plot_options, text='Pokazuj Tętno',variable=plot_ht, onvalue=1, offvalue=0)
-checkbox_show_ht.pack()
-button_draw_plot = tk.Button(plot_options, text="Rysuj wykres", command=lambda: draw_plot(plot_sp.get(),plot_dp.get(),plot_ht.get()))
-button_draw_plot.pack()
+plot_options = tk.LabelFrame(frame_left, text="Opcje wykresu", padx=10, pady=10)
+plot_options.grid(row=2, column=0, sticky="w"+"e")
+checkbox_show_sp = tk.Checkbutton(plot_options, text='Pokazuj Ciś. Sk.', variable=plot_sp, onvalue=1, offvalue=0)
+checkbox_show_sp.pack(anchor="w")
+checkbox_show_dp = tk.Checkbutton(plot_options, text='Pokazuj Ciś. Roz.', variable=plot_dp, onvalue=1, offvalue=0)
+checkbox_show_dp.pack(anchor="w")
+checkbox_show_ht = tk.Checkbutton(plot_options, text='Pokazuj Tętno', variable=plot_ht, onvalue=1, offvalue=0)
+checkbox_show_ht.pack(anchor="w")
+button_draw_plot = tk.Button(plot_options, text="Rysuj wykres", command=lambda: draw_plot(plot_sp.get(),plot_dp.get(), plot_ht.get()))
+button_draw_plot.pack(fill="x")
 
 
 # pokazywanie wynikow w tym samym oknie
-button_show_db = tk.Button(main_frame, text="Pokaz zawartość bazy", command=lambda: show_main_db(show_frame,show_frame_text))
+button_show_db = tk.Button(frame_right, text="Pokaz zawartość bazy", command=lambda: show_main_db(show_frame, show_frame_text))
 button_show_db.pack()
-show_frame = tk.LabelFrame(main_frame, text="Pomiary ciśnienia", padx=10, pady=10)
-show_frame.pack(fill='both',expand="yes")
+show_frame = tk.LabelFrame(frame_right, text="Pomiary ciśnienia", padx=10, pady=10)
+show_frame.pack()
 show_frame_text = tk.StringVar()
 show_frame_text.set(str(show_db))
 # left = tk.Label(show_frame, textvariable=show_frame_text)
